18dec67b90ae01a4b343bfae88791de120f2d493
瀧澤匠子 <takizawashoko@takizawatakumikonoMacBook-Air.local>
2025-06-27 15:57:12 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# ビルド用イメージ
+FROM gradle:8.0.2-jdk17 AS build
+WORKDIR /home/gradle/project
+COPY --chown=gradle:gradle . .
+RUN gradle bootJar --no-daemon
+
+# 実行用イメージ
+FROM eclipse-temurin:17-jdk
+WORKDIR /app
+COPY --from=build /home/gradle/project/build/libs/*.jar app.jar
+
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/demo/Fortune.java
+++ src/main/java/com/example/demo/Fortune.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,13 @@ public class Fortune {
 		} else {
 			return "misfortune.html";
 		}
+	}
+	
+	@Controller
+	public class FortuneController {
+	    @GetMapping("/")
+	    public String index() {
+	        return "index"; // .htmlは不要
+	    }
 	}
 }
